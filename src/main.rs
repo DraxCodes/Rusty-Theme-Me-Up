81fdc5bb4babca4d4a9ba74bd724ff6677de8b94
Joel Parkinson <joelparkinson@live.co.uk>
2021-01-02 17:57:39 +0000

    Implement Json deserialization for the wallhaven result

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use reqwest::header;
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,7 +10,52 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .send()
         .await?;
     
-    println!("{:#?}", res);
+    let results: WallhavenResult = res.json().await?;
+    
+    println!("{:#?}", results);
 
     Ok(())
+}
+
+#[derive(Deserialize, Debug)]
+struct WallhavenResult {
+    data: Vec<WallpaperResult>,
+    meta: Meta
+}
+
+#[derive(Deserialize, Debug)]
+struct WallpaperResult {
+    id: String,
+    url: String,
+    short_url: String,
+    views: i32,
+    favorites: i32,
+    source: String,
+    purity: String,
+    category: String,
+    dimension_x: i32,
+    dimension_y: i32,
+    resolution: String,
+    ratio: String,
+    file_size: i32,
+    file_type: String,
+    created_at: String,
+    colors: Vec<String>,
+    path: String,
+    thumbs: Thumbs,
+}
+
+#[derive(Deserialize, Debug)]
+struct Thumbs {
+    large: String,
+    original: String,
+    small: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Meta {
+    current_page: i32,
+    last_page: i32,
+    per_page: i32,
+    total: i32,
 }
